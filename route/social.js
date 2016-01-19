d72bcf20ebf29470eb7b12f2fd60aec16320c7fa
Guilherme Vieira <super.driver.512@gmail.com>
2016-01-19 06:45:42 -0200

    WIP.

--- route/social.js
+++ route/social.js
@@ -5,5 +5,20 @@ let app = $require('mod/app');
 let templ = $require('templ');
 
 app.get('/social', function(req, res) {
-    res.send(templ.social());
+    res.send(templ.timeline({
+        timelineEvents: [
+            {
+                actor: {
+                    name: 'Guilherme Prá Vieira',
+                    url: 'https://placeholder',
+                    avatarUrl: 'img/profilePicture.png',
+                },
+                action: {
+                    description: 'tweeted',
+                },
+                dateDescription: 'January 19th at 6:26 am',
+                body: 'This is just a test.',
+            }
+        ],
+    }));
 });
